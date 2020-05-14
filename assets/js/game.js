8e9162ab9cdafda22ad1911d007e78fec5a3f31e
Taylor Remigi <tayremigi@gmail.com>
2020-05-14 17:29:39 -0600

    added while loop to prompt for player name until filled

--- assets/js/game.js
+++ assets/js/game.js
@@ -159,9 +159,22 @@ var shop = function () {
     }
 };
 
+// functon to set name
+var getPlayerName = function() {
+    var name = "";
+    
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+
+    return name;
+};
+
 // player info object
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
