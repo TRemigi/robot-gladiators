28329248791ce9484dbb2069c34453806d3e717d
Taylor Remigi <tayremigi@gmail.com>
2020-05-14 18:32:28 -0600

    changed store selection input to numbers instead of strings

--- assets/js/game.js
+++ assets/js/game.js
@@ -146,21 +146,20 @@ var endGame = function () {
 var shop = function () {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', OR 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
+    // make prompt string return into integer 
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     // use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL": // case will fall through to next one because there is no break
-        case "refill":
+        case 1: // case will fall through to next one because there is no break
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             // do nothing, so function will end
             break;
